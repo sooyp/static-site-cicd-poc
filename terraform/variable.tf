bd468a511dbd54f5368841fb114b44cc0ad7741c
sooyp <57872154+sooyp@users.noreply.github.com>
2025-05-09 17:06:52 +0100

    Create variable.tf

---
+++ terraform/variable.tf
@@ -0,0 +1,27 @@
+# Define a variable for the environment name ('dev', 'prod')
+variable "environment" {
+  description = "The environment for the deployment"
+  type        = string
+  default     = "dev"
+}
+
+# Define a variable for the S3 bucket name
+variable "s3_bucket_name" {
+  description = "The name of the S3 bucket to be created"
+  type        = string
+  default     = "my-static-website-${var.environment}"
+}
+
+# Define a variable for the static website index document
+variable "website_index_document" {
+  description = "The index document for the static website"
+  type        = string
+  default     = "index.html"
+}
+
+# Define a variable for the static website error document
+variable "website_error_document" {
+  description = "The error document for the static website"
+  type        = string
+  default     = "error.html"
+}
