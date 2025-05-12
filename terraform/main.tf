25860fac7c086ed8d77409c081ddcf3321eb354e
sooyp <57872154+sooyp@users.noreply.github.com>
2025-05-12 14:46:48 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,13 +4,47 @@ provider "aws" {
 
 resource "aws_s3_bucket" "static_website" {
   bucket = var.s3_bucket_name
-  acl    = "public-read"
+
+  tags = {
+    Environment = var.environment
+  }
 
   website {
     index_document = var.website_index_document
-    # Specify the error document for the website
     error_document = var.website_error_document
   }
+
+  # Enable bucket ownership enforcement
+  ownership_controls {
+    rule {
+      object_ownership = "BucketOwnerEnforced"
+    }
+  }
+
+  # Block all ACLs
+  public_access_block {
+    block_public_acls       = true
+    block_public_policy     = false
+    ignore_public_acls      = true
+    restrict_public_buckets = false
+  }
+}
+
+resource "aws_s3_bucket_policy" "static_website" {
+  bucket = aws_s3_bucket.static_website.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.static_website.arn}/*"
+      }
+    ]
+  })
 }
 
 resource "aws_s3_bucket_object" "static_website_files" {
@@ -19,5 +53,5 @@ resource "aws_s3_bucket_object" "static_website_files" {
   bucket = aws_s3_bucket.static_website.bucket
   key    = each.value
   source = "./static-site/${each.value}"
-  acl    = "public-read"
 }
+
