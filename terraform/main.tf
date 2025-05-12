a64db2ca07e31f7ca718e63ba76ee15375e0e486
sooyp <57872154+sooyp@users.noreply.github.com>
2025-05-12 14:56:28 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,15 @@ provider "aws" {
   region = var.aws_region
 }
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
+    }
+  }
+}
+
 resource "aws_s3_bucket" "static_website" {
   bucket = var.s3_bucket_name
 
